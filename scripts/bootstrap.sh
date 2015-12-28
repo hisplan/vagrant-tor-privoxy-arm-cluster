c32f394883f9b777a7d29f87dd7f0d7c5a842a5b
hisplan <jaeyoung.chun@gmail.com>
2015-12-28 17:30:21 -0500

    Minor fix

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -34,7 +34,7 @@ sudo apt-get -y install tor-arm
 
 # misc (lxml, beautifulsoup4, pytorctl)
 
-sudo apt-get install libxml2-dev libxslt-dev python-dev
+sudo apt-get -y install libxml2-dev libxslt-dev python-dev
 sudo pip install lxml
 
 sudo pip install beautifulsoup4
